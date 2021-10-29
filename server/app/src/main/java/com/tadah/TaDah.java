57bcde5db0545aaea8d8b5d11cd76bb9dfc91d0a
daeungkim <daeungkim93@gmail.com>
2021-10-29 14:23:05 +0900

    :necktie: 메인메서드 작성
    스프링 어플리케이션 실행시키는 메서드 및 실행여부 체크하는 테스트 작성

--- server/app/src/main/java/com/tadah/TaDah.java
+++ server/app/src/main/java/com/tadah/TaDah.java
@@ -1,6 +1,11 @@
 package com.tadah;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class TaDah {
     public static void main(String[] args) {
+        SpringApplication.run(TaDah.class, args);
     }
 }
