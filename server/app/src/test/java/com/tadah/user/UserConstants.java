e54a8f170b35e148b69e14883baf01f2dc84301e
daeungkim <daeungkim93@gmail.com>
2021-11-09 09:14:10 +0900

    :necktie: DTO 추가
    Controller에서 필요하므로 DTO 추가하였음

--- server/app/src/test/java/com/tadah/user/UserConstants.java
+++ server/app/src/test/java/com/tadah/user/UserConstants.java
@@ -2,6 +2,7 @@ package com.tadah.user;
 
 import com.tadah.user.domain.UserType;
 import com.tadah.user.domain.entities.User;
+import com.tadah.user.dto.UserData;
 
 /**
  * 테스트에 반복적으로 사용되는 상수값 정의
@@ -12,4 +13,6 @@ public interface UserConstants {
     String PASSWORD = "password";
     User DRIVER = new User(EMAIL, NAME, PASSWORD, UserType.DRIER);
     User RIDER = new User(EMAIL, NAME, PASSWORD, UserType.RIDER);
+    UserData DRIVER_USER_DATA = new UserData(EMAIL, NAME, PASSWORD, UserType.DRIER);
+    UserData RIDER_USER_DATA = new UserData(EMAIL, NAME, PASSWORD, UserType.RIDER);
 }
