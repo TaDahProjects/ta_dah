a0a8c6c7c16b7f8f7671b39bf9644910c318f6c9
daeungkim <daeungkim93@gmail.com>
2021-11-09 09:19:36 +0900

    :necktie: UserController 작성
    UserController 작성

---
+++ server/app/src/main/java/com/tadah/user/controllers/UserController.java
@@ -0,0 +1,39 @@
+package com.tadah.user.controllers;
+
+import com.tadah.user.applications.UserService;
+import com.tadah.user.domain.entities.User;
+import com.tadah.user.dto.UserData;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * User 관련 요청을 담당한다.
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("/users")
+public final class UserController {
+    private final UserService userService;
+
+    public UserController(final UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * User를 생성하고 리턴한다.
+     *
+     * @param userData 생성할 User 데이터
+     * @return 생성한 User
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@RequestBody final UserData userData) {
+        final User user = userData.toEntity();
+        return userService.saveUser(user);
+    }
+}
