ee93ce0533379e9df8391717be23400036aa2e80
daeungkim <daeungkim93@gmail.com>
2021-11-15 17:05:42 +0900

    :necktie: JWT 검증 기능 구현
    JWT 검증 기능 구현

--- server/app/src/main/java/com/tadah/user/utils/JwtUtil.java
+++ server/app/src/main/java/com/tadah/user/utils/JwtUtil.java
@@ -1,6 +1,8 @@
 package com.tadah.user.utils;
 
+import com.tadah.common.exceptions.InvalidTokenException;
 import com.tadah.user.exceptions.InvalidClaimDataException;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -36,5 +38,24 @@ public final class JwtUtil {
             .claim(CLAIM_NAME, claimData)
             .signWith(key)
             .compact();
+    }
+
+    /**
+     * 토큰을 디코딩하여 속성 정보를 리턴한다.
+     *
+     * @param token JWT
+     * @return JWT Claim(속성 정보)
+     * @throws InvalidTokenException 토큰이 유효하지 않은 경우
+     */
+    public Claims decode(final String token) {
+        try {
+            return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        } catch (Exception exception) {
+            throw new InvalidTokenException();
+        }
     }
 }
