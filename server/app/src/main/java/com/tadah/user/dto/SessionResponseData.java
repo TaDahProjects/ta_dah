4a307604d1a7249c5d61ea769a2802e050ceccf8
daeungkim <daeungkim93@gmail.com>
2021-11-16 23:33:58 +0900

    :necktie: 로그인 엔드포인트 작성
    로그인 엔드포인트 작성

---
+++ server/app/src/main/java/com/tadah/user/dto/SessionResponseData.java
@@ -0,0 +1,12 @@
+package com.tadah.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Generated;
+import lombok.Getter;
+
+@Generated
+@Getter
+@AllArgsConstructor
+public final class SessionResponseData {
+    private final String token;
+}
