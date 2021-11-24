759d7e5b01336d4d8e1a6857efd140803cd6020f
daeungkim <daeungkim93@gmail.com>
2021-11-24 22:12:12 +0900

    :white_check_mark: UserServiceTest 수정
    UserServiceTest의 PasswordEncoder encode 메서드 모킹으로 인해
    DIRVER 개체의 비밀번호가 변경되어 UserRepository save 메서드 테스트가 실패하는 문제가 발생하였음
    따라서 PasswordEncoder 모킹을 하지 않도록 변경하였음

--- server/app/src/test/java/com/tadah/user/applications/UserServiceTest.java
+++ server/app/src/test/java/com/tadah/user/applications/UserServiceTest.java
@@ -8,37 +8,31 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static com.tadah.user.UserConstants.DRIVER;
 import static com.tadah.user.UserConstants.EMAIL;
-import static com.tadah.user.UserConstants.ENCODED_PASSWORD;
 import static com.tadah.user.UserConstants.PASSWORD;
 import static com.tadah.user.UserConstants.RIDER;
+import static com.tadah.user.domain.entities.UserTest.PASSWORD_ENCODER;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.atMostOnce;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @DisplayName("UserService 클래스")
-@ExtendWith(MockitoExtension.class)
 public final class UserServiceTest {
-    @Mock
-    private UserRepository userRepository;
+    private final UserService userService;
+    private final UserRepository userRepository;
 
-    @Mock
-    private PasswordEncoder passwordEncoder;
-
-    @InjectMocks
-    private UserService userService;
+    public UserServiceTest() {
+        this.userRepository = mock(UserRepository.class);
+        this.userService = new UserService(userRepository, PASSWORD_ENCODER);
+    }
 
     @Nested
     @DisplayName("registerUser 메서드는")
@@ -58,16 +52,12 @@ public final class UserServiceTest {
         public final class Context_validEmail {
             @BeforeEach
             private void beforeEach() {
-                when(passwordEncoder.encode(anyString()))
-                    .thenReturn(ENCODED_PASSWORD);
                 when(userRepository.save(any(User.class)))
                     .thenReturn(DRIVER);
             }
 
             @AfterEach
             private void afterEach() {
-                verify(passwordEncoder, atMostOnce())
-                    .encode(anyString());
                 verify(userRepository, atMostOnce())
                     .save(any(User.class));
             }
@@ -91,8 +81,6 @@ public final class UserServiceTest {
 
             @AfterEach
             private void afterEach() {
-                verify(passwordEncoder, times(0))
-                    .encode(anyString());
                 verify(userRepository, times(0))
                     .save(any(User.class));
             }
