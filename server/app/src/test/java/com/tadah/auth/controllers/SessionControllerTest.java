e318eccb967980a7b7325c915f14fb0b82022869
daeungkim <daeungkim93@gmail.com>
2021-11-29 20:16:02 +0900

    :recycle: SessionControllerTest 수정
    ParameterizedTest로 변경하였음

--- server/app/src/test/java/com/tadah/auth/controllers/SessionControllerTest.java
+++ server/app/src/test/java/com/tadah/auth/controllers/SessionControllerTest.java
@@ -5,15 +5,12 @@ import com.tadah.auth.dtos.SessionResponseData;
 import com.tadah.auth.exceptions.LoginFailException;
 import com.tadah.auth.utils.JwtUtil;
 import com.tadah.common.dtos.ErrorResponse;
-import com.tadah.user.domains.entities.User;
 import com.tadah.user.domains.repositories.UserRepository;
 import com.tadah.user.domains.repositories.infra.JpaUserRepository;
 import com.tadah.utils.Parser;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -80,46 +77,49 @@ public final class SessionControllerTest {
         }
 
         @Nested
-        @DisplayName("이메일에 해당하는 사용자가 없는 경우")
-        public final class Context_invalidEmail {
-            @Test
-            @DisplayName("로그인에 실패하였음을 알려준다.")
-            public void it_notifies_that_the_user_login_failed() throws Exception {
-                subject(Parser.toJson(new SessionRequestData("invalid" + EMAIL, PASSWORD)))
-                    .andExpect(status().isBadRequest())
-                    .andExpect(content().string(Parser.toJson(new ErrorResponse(SESSION_URL, HttpMethod.POST.toString(), new LoginFailException().getMessage()))));
+        @DisplayName("입력데이터가 잘못된 경우")
+        @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+        public final class Context_invalidInputData {
+            private Stream<Arguments> methodSource() throws Exception {
+                return Stream.of(
+                    Arguments.of(
+                        Parser.toJson(new SessionRequestData("invalid" + EMAIL, PASSWORD)),
+                        Parser.toJson(new ErrorResponse(SESSION_URL, HttpMethod.POST.toString(), new LoginFailException().getMessage()))
+                    ),
+                    Arguments.of(
+                        Parser.toJson(new SessionRequestData(EMAIL, PASSWORD + "invalid")),
+                        Parser.toJson(new ErrorResponse(SESSION_URL, HttpMethod.POST.toString(), new LoginFailException().getMessage()))
+                    )
+                );
             }
-        }
 
-        @Nested
-        @DisplayName("비밀번호가 일치하지 않은 경우")
-        public final class Context_invalidPassword {
-            @Test
+            @MethodSource("methodSource")
             @DisplayName("로그인에 실패하였음을 알려준다.")
-            public void it_notifies_that_the_user_login_failed() throws Exception {
-                subject(Parser.toJson(new SessionRequestData(EMAIL, PASSWORD + "invalid")))
+            @ParameterizedTest(name = "input=\"{0}\" output=\"{1}\"")
+            public void it_notifies_that_the_user_login_failed(final String input, final String output) throws Exception {
+                subject(input)
                     .andExpect(status().isBadRequest())
-                    .andExpect(content().string(Parser.toJson(new ErrorResponse(SESSION_URL, HttpMethod.POST.toString(), new LoginFailException().getMessage()))));
+                    .andExpect(content().string(output));
             }
         }
 
         @Nested
         @DisplayName("로그인 정보가 일치하는 경우")
+        @TestInstance(TestInstance.Lifecycle.PER_CLASS)
         public final class Context_validLoginInfo {
-            private String token;
-
-            @BeforeEach
-            private void beforeEach() {
-                final User savedUser = userRepository.save(USER);
-                this.token = jwtUtil.encode(savedUser.getId());
+            private Stream<Arguments> methodSource() throws Exception {
+                final Long userId = userRepository.save(USER).getId();
+                final String token = jwtUtil.encode(userId);
+                return Stream.of(Arguments.of(Parser.toJson(new SessionRequestData(EMAIL, PASSWORD)), Parser.toJson(new SessionResponseData(token))));
             }
 
-            @Test
             @DisplayName("토큰을 리턴한다.")
-            public void it_returns_a_token() throws Exception {
-                subject(Parser.toJson(new SessionRequestData(EMAIL, PASSWORD)))
+            @MethodSource("methodSource")
+            @ParameterizedTest(name = "input=\"{0}\" output=\"{1}\"")
+            public void it_returns_a_token(final String input, final String output) throws Exception {
+                subject(input)
                     .andExpect(status().isCreated())
-                    .andExpect(content().string(Parser.toJson(new SessionResponseData(token))));
+                    .andExpect(content().string(output));
             }
         }
 
@@ -129,25 +129,52 @@ public final class SessionControllerTest {
         public final class Context_invalidData {
             private Stream<Arguments> methodSource() throws Exception {
                 return Stream.of(
-                    Arguments.of(Parser.toJson(new SessionRequestData(null, PASSWORD)), "이메일이 입력되지 않았습니다."),
-                    Arguments.of(Parser.toJson(new SessionRequestData("", PASSWORD)), "유효하지 않은 이메일 형식입니다."),
-                    Arguments.of(Parser.toJson(new SessionRequestData(INVALID_EMAIL, PASSWORD)), "유효하지 않은 이메일 형식입니다."),
-                    Arguments.of(Parser.toJson(new SessionRequestData(EMAIL, null)), "비밀번호가 입력되지 않았습니다."),
-                    Arguments.of(Parser.toJson(new SessionRequestData(EMAIL, "")), "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상의 비밀번호를 입력해야합니다."),
-                    Arguments.of(Parser.toJson(new SessionRequestData(EMAIL, INVALID_PASSWORD_LOWER_CASE)), "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상의 비밀번호를 입력해야합니다."),
-                    Arguments.of(Parser.toJson(new SessionRequestData(EMAIL, INVALID_PASSWORD_UPPER_CASE)), "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상의 비밀번호를 입력해야합니다."),
-                    Arguments.of(Parser.toJson(new SessionRequestData(EMAIL, INVALID_PASSWORD_NUMBER)), "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상의 비밀번호를 입력해야합니다."),
-                    Arguments.of(Parser.toJson(new SessionRequestData(EMAIL, INVALID_PASSWORD_SPECIAL_CASE)), "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상의 비밀번호를 입력해야합니다.")
+                    Arguments.of(
+                        Parser.toJson(new SessionRequestData(null, PASSWORD)),
+                        Parser.toJson(new ErrorResponse(SESSION_URL, HttpMethod.POST.toString(), "이메일이 입력되지 않았습니다."))
+                    ),
+                    Arguments.of(
+                        Parser.toJson(new SessionRequestData("", PASSWORD)),
+                        Parser.toJson(new ErrorResponse(SESSION_URL, HttpMethod.POST.toString(), "유효하지 않은 이메일 형식입니다."))
+                    ),
+                    Arguments.of(
+                        Parser.toJson(new SessionRequestData(INVALID_EMAIL, PASSWORD)),
+                        Parser.toJson(new ErrorResponse(SESSION_URL, HttpMethod.POST.toString(), "유효하지 않은 이메일 형식입니다."))
+                    ),
+                    Arguments.of(
+                        Parser.toJson(new SessionRequestData(EMAIL, null)),
+                        Parser.toJson(new ErrorResponse(SESSION_URL, HttpMethod.POST.toString(), "비밀번호가 입력되지 않았습니다."))
+                    ),
+                    Arguments.of(
+                        Parser.toJson(new SessionRequestData(EMAIL, "")),
+                        Parser.toJson(new ErrorResponse(SESSION_URL, HttpMethod.POST.toString(), "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상의 비밀번호를 입력해야합니다."))
+                    ),
+                    Arguments.of(
+                        Parser.toJson(new SessionRequestData(EMAIL, INVALID_PASSWORD_LOWER_CASE)),
+                        Parser.toJson(new ErrorResponse(SESSION_URL, HttpMethod.POST.toString(), "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상의 비밀번호를 입력해야합니다."))
+                    ),
+                    Arguments.of(
+                        Parser.toJson(new SessionRequestData(EMAIL, INVALID_PASSWORD_UPPER_CASE)),
+                        Parser.toJson(new ErrorResponse(SESSION_URL, HttpMethod.POST.toString(), "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상의 비밀번호를 입력해야합니다."))
+                    ),
+                    Arguments.of(
+                        Parser.toJson(new SessionRequestData(EMAIL, INVALID_PASSWORD_NUMBER)),
+                        Parser.toJson(new ErrorResponse(SESSION_URL, HttpMethod.POST.toString(), "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상의 비밀번호를 입력해야합니다."))
+                    ),
+                    Arguments.of(
+                        Parser.toJson(new SessionRequestData(EMAIL, INVALID_PASSWORD_SPECIAL_CASE)),
+                        Parser.toJson(new ErrorResponse(SESSION_URL, HttpMethod.POST.toString(), "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상의 비밀번호를 입력해야합니다."))
+                    )
                 );
             }
 
             @MethodSource("methodSource")
             @DisplayName("입력 데이터가 잘못되었음을 알려준다.")
-            @ParameterizedTest(name = "input=\"{0}\" errorMessage=\"{1}\"")
-            public void it_notifies_that_input_data_is_invalid(final String input, final String errorMessage) throws Exception {
+            @ParameterizedTest(name = "input=\"{0}\" output=\"{1}\"")
+            public void it_notifies_that_input_data_is_invalid(final String input, final String output) throws Exception {
                 subject(input)
                     .andExpect(status().isBadRequest())
-                    .andExpect(content().string(Parser.toJson(new ErrorResponse(SESSION_URL, HttpMethod.POST.toString(), errorMessage))));
+                    .andExpect(content().string(output));
             }
         }
     }
