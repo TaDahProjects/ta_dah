41dea9980712cbbb9ff21840c9669706ab057205
daeungkim <daeungkim93@gmail.com>
2021-11-29 17:03:51 +0900

    :necktie: Spring Security에 Role 적용
    Spring Security에 Role 적용하였음

--- server/app/src/main/java/com/tadah/auth/filters/JwtAuthenticationFilter.java
+++ server/app/src/main/java/com/tadah/auth/filters/JwtAuthenticationFilter.java
@@ -1,7 +1,9 @@
 package com.tadah.auth.filters;
 
 import com.tadah.auth.applications.AuthenticationService;
+import com.tadah.auth.applications.AuthorizationService;
 import com.tadah.auth.authentication.UserAuthentication;
+import com.tadah.auth.domain.entities.Role;
 import com.tadah.user.domain.entities.User;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
@@ -14,6 +16,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * 인증을 진행한다.
@@ -22,11 +25,15 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String TOKEN_PREFIX = "Bearer ";
 
+    private final AuthorizationService authorizationService;
     private final AuthenticationService authenticationService;
     public JwtAuthenticationFilter(
-        final AuthenticationManager authenticationManager, final AuthenticationService authenticationService
+        final AuthenticationManager authenticationManager,
+        final AuthorizationService authorizationService,
+        final AuthenticationService authenticationService
         ) {
         super(authenticationManager);
+        this.authorizationService = authorizationService;
         this.authenticationService = authenticationService;
     }
 
@@ -39,7 +46,8 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
         if (authorizationHeader != null) {
             final String token = authorizationHeader.substring(TOKEN_PREFIX.length());
             final User user = authenticationService.verifyToken(token);
-            final Authentication authentication = new UserAuthentication(user);
+            final List<Role> roles = authorizationService.list(user.getId());
+            final Authentication authentication = new UserAuthentication(user, roles);
 
             final SecurityContext context = SecurityContextHolder.getContext();
             context.setAuthentication(authentication);
