5b8d51a51bb5795c3b836cb6f4c155beeee25cd7
daeungkim <daeungkim93@gmail.com>
2021-12-08 15:05:08 +0900

    :necktie: 차량 운행 시작 api 작성
    차량 운행 시작 api 작성

--- server/app/src/main/java/com/tadah/vehicle/controllers/VehicleController.java
+++ server/app/src/main/java/com/tadah/vehicle/controllers/VehicleController.java
@@ -5,18 +5,23 @@ import com.tadah.auth.domains.entities.Role;
 import com.tadah.user.domains.entities.User;
 import com.tadah.vehicle.applications.VehicleService;
 import com.tadah.vehicle.domains.entities.Vehicle;
+import com.tadah.vehicle.dtos.DrivingRequestData;
 import com.tadah.vehicle.exceptions.VehicleAlreadyExistException;
+import com.tadah.vehicle.exceptions.VehicleNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 /**
- * 차량 생성을 수행한다.
+ * 차량 생성, 운행여부 수정, 위치 업데이트를 담당한다.
  */
 @CrossOrigin
 @RestController
@@ -51,5 +56,25 @@ public class VehicleController {
         final Vehicle vehicle = toEntity(user);
         authorizationService.create(new Role(user.getId(), DRIVER_ROLE));
         vehicleService.create(vehicle);
+    }
+
+    /**
+     * 차량 운행을 시작한다.
+     *
+     * @param user 차량의 소유자
+     * @param drivingRequestData 차량 운행 시작 위치 정보
+     * @throws VehicleNotFoundException 사용자가 차량을 소유하고 있지 않은 경우
+     */
+    @PostMapping("/driving")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("isAuthenticated() and hasAuthority('DRIVER')")
+    public void startDriving(
+        @AuthenticationPrincipal final User user,
+        @RequestBody @Valid final DrivingRequestData drivingRequestData
+    ) {
+        final Long userId = user.getId();
+        final Double latitude = drivingRequestData.getLatitude();
+        final Double longitude = drivingRequestData.getLongitude();
+        vehicleService.startDriving(userId, latitude, longitude);
     }
 }
--- server/app/src/main/java/com/tadah/vehicle/controllers/VehicleControllerAdvice.java
+++ server/app/src/main/java/com/tadah/vehicle/controllers/VehicleControllerAdvice.java
@@ -2,6 +2,7 @@ package com.tadah.vehicle.controllers;
 
 import com.tadah.common.dtos.ErrorResponse;
 import com.tadah.vehicle.exceptions.VehicleAlreadyExistException;
+import com.tadah.vehicle.exceptions.VehicleNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,11 +16,36 @@ import javax.servlet.http.HttpServletRequest;
  */
 @RestControllerAdvice
 public final class VehicleControllerAdvice {
+    /**
+     * 사용자가 이미 차량을 등록한 경우
+     * 해당 에외가 어디서 던져졌는지 리턴한다.
+     *
+     * @param request 예외가 던져진 http 요청
+     * @param exception 컨트롤러에서 던져진 예외
+     * @return 던져진 예외의 내용 및 위치
+     */
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(VehicleAlreadyExistException.class)
     public ErrorResponse handleVehicleAlreadyExistException(
         final HttpServletRequest request, final VehicleAlreadyExistException exception
+    ) {
+        return new ErrorResponse(request, exception.getMessage());
+    }
+
+    /**
+     * 사용자가 소유한 차량을 찾을 수 없는 경우
+     * 해당 예외가 어디서 던져졌는지 리턴한다.
+     *
+     * @param request 예외가 던져진 http 요청
+     * @param exception 컨트롤러에서 던져진 예외
+     * @return 던져진 예외의 내용 및 위치
+     */
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(VehicleNotFoundException.class)
+    public ErrorResponse handleVehicleNotFoundException(
+        final HttpServletRequest request, final VehicleNotFoundException exception
     ) {
         return new ErrorResponse(request, exception.getMessage());
     }
