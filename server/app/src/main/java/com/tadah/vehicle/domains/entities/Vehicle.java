9aeaad3a80a4067966b0489e5463a24b439ae840
daeungkim <daeungkim93@gmail.com>
2021-12-08 10:25:34 +0900

    :necktie: 차량의 운행상태 변경 메서드 작성
    차량의 운행상태 변경 메서드 작성

--- server/app/src/main/java/com/tadah/vehicle/domains/entities/Vehicle.java
+++ server/app/src/main/java/com/tadah/vehicle/domains/entities/Vehicle.java
@@ -3,12 +3,14 @@ package com.tadah.vehicle.domains.entities;
 import com.tadah.common.annotations.Generated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * 차량 데이터를 정의한다.
+ */
 @Generated
 @Entity
 @Getter
@@ -20,15 +22,31 @@ public final class Vehicle {
 
     private Long userId;
 
-    @Setter
     private Double latitude;
 
-    @Setter
     private Double longitude;
 
-    private boolean isRunning = false;
+    private boolean isDriving = false;
 
     public Vehicle(final Long userId) {
         this.userId = userId;
+    }
+
+    /**
+     * 차량의 운행상태를 변경한다.
+     */
+    public void toggleDriving() {
+        isDriving = !isDriving;
+    }
+
+    /**
+     * 차량의 위치를 업데이트한다.
+     *
+     * @param latitude 위도
+     * @param longitude 경도
+     */
+    public void updateLocation(final Double latitude, final Double longitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
     }
 }
--- server/app/src/test/java/com/tadah/vehicle/domains/entities/VehicleTest.java
+++ server/app/src/test/java/com/tadah/vehicle/domains/entities/VehicleTest.java
@@ -1,12 +1,66 @@
 package com.tadah.vehicle.domains.entities;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 
 import static com.tadah.user.domains.entities.UserTest.USER_ID;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DisplayName("Vehicle 클래스")
 public final class VehicleTest {
     public static final Double LATITUDE = 37.487964946;
     public static final Double LONGITUDE = 127.065536349;
     public static final Vehicle VEHICLE = new Vehicle(USER_ID);
+
+    public static Double getLatitude() {
+        return VEHICLE.getLatitude() == null ? LATITUDE : VEHICLE.getLatitude() + 1;
+    }
+
+    public static Double getLongitude() {
+        return VEHICLE.getLongitude() == null ? LONGITUDE : VEHICLE.getLongitude() + 1;
+    }
+
+    @Nested
+    @DisplayName("toggleDriving 메서드는")
+    public final class Describe_toggleDriving {
+        private void subject() {
+            VEHICLE.toggleDriving();
+        }
+
+        @RepeatedTest(2)
+        @DisplayName("차량의 운행상태를 변경한다.")
+        public void it_changes_the_driving_status_of_the_vehicle() {
+            final boolean isDriving = VEHICLE.isDriving();
+
+            subject();
+
+            assertThat(VEHICLE.isDriving())
+                .isNotEqualTo(isDriving);
+        }
+    }
+
+    @Nested
+    @DisplayName("updateLocation 메서드는")
+    public final class Describe_updateLocation {
+        private void subject(final Double latitude, final Double longitude) {
+            VEHICLE.updateLocation(latitude, longitude);
+        }
+
+        @Test
+        @DisplayName("차량의 위치를 업데이트한다.")
+        public void it_updates_the_location_of_the_vehicle() {
+            final Double latitude = getLatitude();
+            final Double longitude = getLongitude();
+
+            subject(latitude, longitude);
+
+            assertThat(VEHICLE.getLatitude())
+                .isEqualTo(latitude);
+
+            assertThat(VEHICLE.getLongitude())
+                .isEqualTo(longitude);
+        }
+    }
 }
