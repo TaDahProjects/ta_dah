f80359bcbf6d2af23d83b1e5ec285626febf52b0
daeungkim <daeungkim93@gmail.com>
2022-01-07 00:41:15 +0900

    :recycle: CommandLineRunner 분리
    Tracker 클래스에서 CommandLineRunner 분리하였음
    -> 테스트 수행 시 IllegalStateException 발생함

--- tracker/app/src/main/java/com/tadah/Tracker.java
+++ tracker/app/src/main/java/com/tadah/Tracker.java
@@ -1,41 +1,11 @@
 package com.tadah;
 
-import com.bedatadriven.jackson.datatype.jts.JtsModule;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import software.amazon.kinesis.common.ConfigsBuilder;
-import software.amazon.kinesis.coordinator.Scheduler;
 
 @SpringBootApplication
-public class Tracker implements CommandLineRunner {
+public class Tracker {
     public static void main(String[] args) {
         SpringApplication.run(Tracker.class, args);
-    }
-
-    private final ApplicationContext applicationContext;
-    public Tracker(@Autowired final ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-   }
-
-    /**
-     * Kinesis 레코드를 수신한다.
-     */
-    @Override
-    public void run(String... args) {
-        final ConfigsBuilder configsBuilder = applicationContext.getBean(ConfigsBuilder.class);
-
-        final Scheduler scheduler = new Scheduler(
-            configsBuilder.checkpointConfig(), configsBuilder.coordinatorConfig(),
-            configsBuilder.leaseManagementConfig(), configsBuilder.lifecycleConfig(),
-            configsBuilder.metricsConfig(), configsBuilder.processorConfig(), configsBuilder.retrievalConfig());
-
-        final Thread schedulerThread = new Thread(scheduler);
-        schedulerThread.setDaemon(true);
-        schedulerThread.start();
     }
 }
--- tracker/app/src/main/java/com/tadah/driving/kinesis/ConsumerProcessorFactory.java
+++ tracker/app/src/main/java/com/tadah/driving/kinesis/ConsumerProcessorFactory.java
@@ -1,13 +1,11 @@
 package com.tadah.driving.kinesis;
 
-import org.springframework.stereotype.Component;
 import software.amazon.kinesis.processor.ShardRecordProcessor;
 import software.amazon.kinesis.processor.ShardRecordProcessorFactory;
 
 /**
  * ConsumerProcessor를 생성한다.
  */
-@Component
 public final class ConsumerProcessorFactory implements ShardRecordProcessorFactory {
     @Override
     public ShardRecordProcessor shardRecordProcessor() {
