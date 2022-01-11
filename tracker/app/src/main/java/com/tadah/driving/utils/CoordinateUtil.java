b4ec63c210230b07f1924f371ca5d7bd36393b87
daeungkim <daeungkim93@gmail.com>
2022-01-11 21:54:51 +0900

    :necktie: 좌표계 변환 기능 구현
    EPSG 5179 -> EPSG 4326 변환 기능 구현

--- tracker/app/src/main/java/com/tadah/driving/utils/CoordinateUtil.java
+++ tracker/app/src/main/java/com/tadah/driving/utils/CoordinateUtil.java
@@ -1,10 +1,11 @@
 package com.tadah.driving.utils;
 
 import org.geolatte.geom.C2D;
+import org.geolatte.geom.G2D;
 import org.geolatte.geom.Geometries;
 import org.geolatte.geom.Point;
+import org.geolatte.geom.crs.CoordinateReferenceSystems;
 import org.geolatte.geom.crs.CrsRegistry;
-import org.geolatte.geom.crs.ProjectedCoordinateReferenceSystem;
 import org.geotools.geometry.jts.JTS;
 import org.geotools.geometry.jts.JTSFactoryFinder;
 import org.geotools.referencing.CRS;
@@ -14,46 +15,56 @@ import org.locationtech.jts.geom.GeometryFactory;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.TransformException;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 /**
  * 좌표계 변환을 담당한다
  */
 @Component
 public final class CoordinateUtil {
-    private static final ProjectedCoordinateReferenceSystem EPSG_5179 = CrsRegistry.getProjectedCoordinateReferenceSystemForEPSG(5179);
-
-    private final MathTransform mathTransform;
+    private final MathTransform toGpsTransForm;
+    private final MathTransform fromGpsTransForm;
     private final GeometryFactory geometryFactory;
 
     public CoordinateUtil() throws FactoryException {
-        final CoordinateReferenceSystem targetCRS = CRS.decode("EPSG:5179");
-        final CoordinateReferenceSystem sourceCRS = CRS.decode("EPSG:4326");
-        mathTransform = CRS.findMathTransform(sourceCRS, targetCRS);
+        final CoordinateReferenceSystem mpsCoordinateSystem = CRS.decode("EPSG:5179");
+        final CoordinateReferenceSystem gpsCoordinateSystem = CRS.decode("EPSG:4326");
+
         geometryFactory = JTSFactoryFinder.getGeometryFactory();
+        toGpsTransForm = CRS.findMathTransform(mpsCoordinateSystem, gpsCoordinateSystem);
+        fromGpsTransForm = CRS.findMathTransform(gpsCoordinateSystem, mpsCoordinateSystem);
+    }
+
+    private Coordinate transForm(final Coordinate coordinate, final MathTransform transform) throws TransformException {
+        final Geometry sourceGeometry = geometryFactory.createPoint(coordinate);
+        final Geometry targetGeometry = JTS.transform(sourceGeometry, transform);
+        return targetGeometry.getCoordinate();
     }
 
     /**
-     * 좌표계 변환을 수행한다
+     * GPS 데이터의 좌표계를 변환한다
      *
      * @param latitude 위도
      * @param longitude 경도
      * @return EPSG 5179 좌표
+     * @throws TransformException 좌표계 변환에 실패한 경우
      */
-    public Optional<Point<C2D>> transform(final Double latitude, final Double longitude) {
-        try {
-            final Coordinate sourceCoordinate = new Coordinate(latitude, longitude);
-            final Geometry sourceGeometry = geometryFactory.createPoint(sourceCoordinate);
+    public Point<C2D> fromGps(final Double latitude, final Double longitude) throws TransformException {
+        final Coordinate coordinate = transForm(new Coordinate(latitude, longitude), fromGpsTransForm);
+        return Geometries.mkPoint(new C2D(coordinate.y, coordinate.x), CrsRegistry.getProjectedCoordinateReferenceSystemForEPSG(5179));
+    }
 
-            final Geometry targetGeometry = JTS.transform(sourceGeometry, mathTransform);
-            final Coordinate targetCoordinate = targetGeometry.getCoordinate();
-
-            return Optional.of(Geometries.mkPoint(new C2D(targetCoordinate.y, targetCoordinate.x), EPSG_5179));
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return Optional.empty();
-        }
+    /**
+     * 위치 정보를 GPS 좌표계로 변환한다
+     *
+     * @param x x
+     * @param y y
+     * @return GPS 좌표
+     * @throws TransformException 좌표계 변환에 실패한 경우
+     */
+    public Point<G2D> toGps(final Double x, final Double y) throws TransformException {
+        final Coordinate coordinate = transForm(new Coordinate(y, x), toGpsTransForm);
+        return Geometries.mkPoint(new G2D(coordinate.y, coordinate.x), CoordinateReferenceSystems.WGS84);
     }
 }
--- tracker/app/src/test/java/com/tadah/dirving/utils/CoordinateUtilTest.java
+++ tracker/app/src/test/java/com/tadah/dirving/utils/CoordinateUtilTest.java
@@ -1,22 +1,23 @@
 package com.tadah.dirving.utils;
 
 import com.tadah.driving.utils.CoordinateUtil;
+import org.assertj.core.data.Offset;
 import org.geolatte.geom.C2D;
+import org.geolatte.geom.G2D;
 import org.geolatte.geom.Point;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.opengis.referencing.FactoryException;
-
-import java.util.Optional;
+import org.opengis.referencing.operation.TransformException;
 
 import static com.tadah.dirving.domains.entities.DrivingTest.POINT;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DisplayName("CoordinateUtil 클래스")
 public final class CoordinateUtilTest {
-    private static final Double LATITUDE = 28.6;
-    private static final Double LONGITUDE = 122.71;
+    private static final Double LATITUDE = 28.6D;
+    private static final Double LONGITUDE = 122.71D;
 
     private final CoordinateUtil coordinateUtil;
 
@@ -25,20 +26,37 @@ public final class CoordinateUtilTest {
     }
 
     @Nested
-    @DisplayName("transform 메서드는")
-    public final class Describe_transform {
-        private Optional<Point<C2D>> subject() {
-            return coordinateUtil.transform(LATITUDE, LONGITUDE);
+    @DisplayName("fromGps 메서드는")
+    public final class Describe_fromGps {
+        private Point<C2D> subject() throws TransformException {
+            return coordinateUtil.fromGps(LATITUDE, LONGITUDE);
         }
 
         @Test
-        @DisplayName("좌표계 변환을 수행한다.")
-        public void it_transform_coordinate_system() {
+        @DisplayName("GPS 데이터의 좌표계를 변환한다")
+        public void it_converts_the_coordinate_system_of_gps_data() throws TransformException {
             assertThat(subject())
-                .isPresent()
-                .get()
                 .matches(point -> point.getPosition().getX() == POINT.getPosition().getX())
                 .matches(point -> point.getPosition().getY() == POINT.getPosition().getY());
+        }
+    }
+
+    @Nested
+    @DisplayName("toGps 메서드는")
+    public final class Describe_toGps {
+        private Point<G2D> subject() throws TransformException {
+            return coordinateUtil.toGps(POINT.getPosition().getX(), POINT.getPosition().getY());
+        }
+
+        @Test
+        @DisplayName("위치정보를 GPS 좌표계로 변환한다")
+        public void it_converts_location_information_into_gps_coordinates() throws TransformException {
+            final Point<G2D> point = subject();
+
+            assertThat(point.getPosition().getLat())
+                .isEqualTo(LATITUDE, Offset.offset(0.01d));
+            assertThat(point.getPosition().getLon())
+                .isEqualTo(LONGITUDE, Offset.offset(0.01d));
         }
     }
 }
