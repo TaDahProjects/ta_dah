f327900bdeaaa702d246c9b544511ee8cbb01f3a
daeungkim <daeungkim93@gmail.com>
2022-02-14 05:56:10 +0900

    :label: KinesisConsumer 타입 변경
    KinesisConsumer 수신 타입 변경(String -> DrivingData)

--- tracker/app/src/main/java/com/tadah/driving/configs/KinesisConsumerConfiguration.java
+++ tracker/app/src/main/java/com/tadah/driving/configs/KinesisConsumerConfiguration.java
@@ -1,11 +1,15 @@
 package com.tadah.driving.configs;
 
 import com.tadah.driving.applications.DrivingService;
+import com.tadah.driving.dtos.DrivingDataProto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Consumer;
 
+/**
+ * Kinesis Stream Consumer를 등록한다
+ */
 @Configuration
 public class KinesisConsumerConfiguration {
     private final DrivingService drivingService;
@@ -15,7 +19,7 @@ public class KinesisConsumerConfiguration {
     }
 
     @Bean
-    public Consumer<String> processDriving() {
+    public Consumer<DrivingDataProto.DrivingData> processDriving() {
         return System.out::println;
     }
 }
