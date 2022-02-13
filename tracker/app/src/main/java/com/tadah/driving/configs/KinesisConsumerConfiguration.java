38e638221f649d303969e7514d5f19cad491b883
daeungkim <daeungkim93@gmail.com>
2022-02-14 08:10:22 +0900

    :necktie: Spring Cloud Stream Consumer 작성
    Spring Cloud Stream Consumer 작성

--- tracker/app/src/main/java/com/tadah/driving/configs/KinesisConsumerConfiguration.java
+++ tracker/app/src/main/java/com/tadah/driving/configs/KinesisConsumerConfiguration.java
@@ -1,10 +1,14 @@
 package com.tadah.driving.configs;
 
 import com.tadah.driving.applications.DrivingService;
+import com.tadah.driving.domains.entities.Driving;
 import com.tadah.driving.dtos.DrivingDataProto;
+import org.geolatte.geom.C2D;
+import org.geolatte.geom.Point;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Optional;
 import java.util.function.Consumer;
 
 /**
@@ -20,6 +24,28 @@ public class KinesisConsumerConfiguration {
 
     @Bean
     public Consumer<DrivingDataProto.DrivingData> processDriving() {
-        return System.out::println;
+        return drivingData -> {
+            final Optional<Driving> drivingOptional = drivingService.get(drivingData.getUserId());
+            if (drivingOptional.isEmpty() && !drivingData.getDrivingStatus().equals(DrivingDataProto.DrivingStatus.START)) {
+                // todo radis pub/sub 연동 후 추가 필요
+            }
+            if (drivingOptional.isPresent() && drivingData.getDrivingStatus().equals(DrivingDataProto.DrivingStatus.START)) {
+                // todo radis pub/sub 연동 후 추가 필요
+            }
+
+            try {
+                final Point<C2D> point = drivingService.transForm(drivingData.getLatitude(), drivingData.getLongitude());
+                // 분기
+                final Driving driving = drivingOptional.orElse(new Driving(drivingData.getUserId(), point));
+
+                switch (drivingData.getDrivingStatus()) {
+                    case START -> drivingService.start(driving);
+                    case DRIVING -> drivingService.update(driving, point);
+                    case STOP -> drivingService.stop(driving, point);
+                }
+            } catch (Exception exception) {
+                // todo radis pub/sub 연동 후 추가 필요
+            }
+        };
     }
 }
