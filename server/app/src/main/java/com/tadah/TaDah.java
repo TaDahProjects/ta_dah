d7ec61ac2df1fe70dce40d5a16cc0ae88202f9f3
daeungkim <daeungkim93@gmail.com>
2022-02-15 10:02:38 +0900

    :seedling: ProtobufMessageConverter 작성 및 등록
    AWS Kinesis Data Stream ProtoBuf 전송시 사용할 메시지 컨버터 정의 및 등록

--- server/app/src/main/java/com/tadah/TaDah.java
+++ server/app/src/main/java/com/tadah/TaDah.java
@@ -3,6 +3,8 @@ package com.tadah;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.converter.ProtobufMessageConverter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -15,5 +17,10 @@ public class TaDah {
     @Bean
     public static PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public static MessageConverter getMessageConverter() {
+        return new ProtobufMessageConverter();
     }
 }
